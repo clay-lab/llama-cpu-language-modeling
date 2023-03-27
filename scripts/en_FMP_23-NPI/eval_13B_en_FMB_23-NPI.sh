2d4656104839d510df828c3593e366aff7b68599
Michael Wilson <michael.a.wilson@yale.edu>
2023-03-27 17:56:13 -0400

    fix json format

---
+++ scripts/en_FMP_23-NPI/eval_13B_en_FMB_23-NPI.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=llama-13B-en_FMP_23-NPI
+#SBATCH --output=joblogs/%x_%j.txt
+#SBATCH --partition=day
+#SBATCH --mem=78G
+#SBATCH --time=02:00:00
+#SBATCH --mail-type=END,FAIL,INVALID_DEPEND
+
+module load miniconda
+
+source activate /gpfs/gibbs/project/frank/ref4/conda_envs/llama
+
+echo "Starting job at `date`"
+echo
+
+time python run_LM.py \
+	--ckpt_dir llama-checkpoints/13B \
+	--tokenizer_path llama-checkpoints/tokenizer.model \
+	--dataset_path data/en_FMP_23-NPI/phillips2021_exp1.txt.gz \
+	--max_batch_size 2
+
+echo
+echo "Finished job at `date`"
---
+++ scripts/en_FMP_23-NPI/eval_7B_en_FMB_23-NPI.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=llama-7B-en_FMP_23-NPI
+#SBATCH --output=joblogs/%x_%j.txt
+#SBATCH --partition=day
+#SBATCH --mem=48G
+#SBATCH --time=01:00:00
+#SBATCH --mail-type=END,FAIL,INVALID_DEPEND
+
+module load miniconda
+
+source activate /gpfs/gibbs/project/frank/ref4/conda_envs/llama
+
+echo "Starting job at `date`"
+echo
+
+time python run_LM.py \
+	--ckpt_dir llama-checkpoints/7B \
+	--tokenizer_path llama-checkpoints/tokenizer.model \
+	--dataset_path data/en_FMP_23-NPI/phillips2021_exp1.txt.gz \
+	--max_batch_size 2
+
+echo
+echo "Finished job at `date`"
